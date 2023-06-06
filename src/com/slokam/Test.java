6811d20b61f888e0e0d33ec59a88cac8a609dc3e
Deepika <jsldeepika10@gmail.com>
2023-06-06 12:47:20 +0530

    Eclipse

--- src/com/slokam/Test.java
+++ src/com/slokam/Test.java
@@ -1,9 +1,13 @@
 package com.slokam;
 
 public class Test {
-
+public void m1()
+{
+	System.out.println("m1 method");
+}
 	public static void main(String[] args) {
-
+		 Test t=new Test();
+		 t.m1();
 	}
 
 }
